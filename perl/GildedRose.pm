b248dd73608573b04c51efa121fb397ed56ae20b
Colin Keith <colin.keith@oracle.com>
2022-10-28 23:07:23 +0100

    replaced text for backstage_pass in code with sub

--- perl/GildedRose.pm
+++ perl/GildedRose.pm
@@ -18,11 +18,16 @@ sub is_aged_brie {
   return $item->{name} eq 'Aged Brie';
 }
 
+sub is_backstage_pass {
+  my $item = shift;
+  return $item->{name} eq 'Backstage passes to a TAFKAL80ETC concert';
+}
+
 sub update_quality {
     my $self = shift;
     for my $item ( $self->items()  ) {
         if ( !is_aged_brie($item)
-            && $item->{name} ne 'Backstage passes to a TAFKAL80ETC concert' )
+          && !is_backstage_pass($item) )
         {
             if ( $item->{quality} > 0 ) {
                 if ( $item->{name} ne 'Sulfuras, Hand of Ragnaros' ) {
@@ -34,8 +39,7 @@ sub update_quality {
             if ( $item->{quality} < 50 ) {
                 $item->{quality} = $item->{quality} + 1;
 
-                if ( $item->{name} eq
-                    'Backstage passes to a TAFKAL80ETC concert' )
+                if ( is_backstage_pass($item) )
                 {
                     if ( $item->{sell_in} < 11 ) {
                         if ( $item->{quality} < 50 ) {
@@ -58,8 +62,7 @@ sub update_quality {
 
         if ( $item->{sell_in} < 0 ) {
             if ( !is_aged_brie($item) ) {
-                if ( $item->{name} ne
-                    'Backstage passes to a TAFKAL80ETC concert' )
+                if ( !is_backstage_pass($item) )
                 {
                     if ( $item->{quality} > 0 ) {
                         if ( $item->{name} ne 'Sulfuras, Hand of Ragnaros' ) {

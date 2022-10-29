b9b69c8229fd8aa33bc8853aa14ac2aa84a004fc
Colin Keith <colin.keith@oracle.com>
2022-10-29 12:05:39 +0100

    add test to ensure basic quality/sell_in degradation works

--- perl/test.pl
+++ perl/test.pl
@@ -15,4 +15,19 @@ subtest 'foo' => sub {
     is( [ $app->items() ]->[0]->{name}, 'foo' );
 };
 
+subtest 'Basic Tests' => sub {
+    my $sell_in_days = 2;
+    my $quality      = 2;
+    my $items = [ GildedRose::Item->new( name => 'foo', sell_in => $sell_in_days, quality => $quality ) ];
+    my $app = GildedRose->new( items => $items );
+    my $check_item = [ $app->items() ]->[0];
+    my $name = $check_item->{name};
+    for my $day ( 0..2 ) {
+      diag("Day: $day");
+      is($check_item->sell_in, $sell_in_days - $day, "Item ${name} sell_in decreased by 1 for $day day(s)" );
+      is($check_item->quality, $quality - $day,      "Item ${name} quality decreased as 1 for $day day(s)" );
+      $app->update_quality();
+    }
+};
+
 done_testing();

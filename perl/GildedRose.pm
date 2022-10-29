dad3b7a6a0c7d182f5444183a84f202c5877c342
Colin Keith <colin.keith@oracle.com>
2022-10-29 11:24:19 +0100

    Move subs to our own GR::Item

--- perl/GildedRose.pm
+++ perl/GildedRose.pm
@@ -14,24 +14,9 @@ sub items {
   return @{ $self->{items} };
 }
 
-sub is_aged_brie {
-  my $item = shift;
-  return $item->{name} eq 'Aged Brie';
-}
-
-sub is_backstage_pass {
-  my $item = shift;
-  return $item->{name} eq 'Backstage passes to a TAFKAL80ETC concert';
-}
-
-sub is_hand_of_ragnaros {
-  my $item = shift;
-  return $item->{name} eq 'Sulfuras, Hand of Ragnaros';
-}
-
 sub item_degrades_over_time {
     my $item = shift;
-    is_hand_of_ragnaros($item) && return;
+    $item->is_hand_of_ragnaros && return;
     $item->{quality} > 0 && $item->{quality}--;
 }
 
@@ -40,16 +25,16 @@ sub update_quality {
     for my $item ( $self->items() ) {
         update_item_quality($item);
 
-        if ( !is_hand_of_ragnaros($item) ) {
+        if ( !$item->is_hand_of_ragnaros ) {
             $item->{sell_in}--;
         }
 
         if ( $item->{sell_in} < 0 ) {
-            if ( !is_aged_brie($item) ) {
-                if ( !is_backstage_pass($item) )
+            if ( !$item->is_aged_brie ) {
+                if ( !$item->is_backstage_pass )
                 {
                     if ( $item->{quality} > 0 ) {
-                        if ( !is_hand_of_ragnaros($item) ) {
+                        if ( !$item->is_hand_of_ragnaros ) {
                             $item->{quality}--;
                         }
                     }
@@ -70,8 +55,8 @@ sub update_quality {
 
 sub update_item_quality {
     my $item = shift;
-    if ( !is_aged_brie($item)
-      && !is_backstage_pass($item) )
+    if ( !$item->is_aged_brie
+      && !$item->is_backstage_pass )
     {
        item_degrades_over_time($item);
     }
@@ -79,7 +64,7 @@ sub update_item_quality {
         if ( $item->{quality} < 50 ) {
             $item->{quality}++;
 
-            if ( is_backstage_pass($item) )
+            if ( $item->is_backstage_pass )
             {
                 if ( $item->{sell_in} < 11 ) {
                     if ( $item->{quality} < 50 ) {

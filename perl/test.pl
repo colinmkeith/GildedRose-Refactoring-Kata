31a3cef9be2c2f0f9ce7bed67f404125cef0a5ff
Colin Keith <colin.keith@oracle.com>
2022-10-29 14:34:17 +0100

    simplified testing by having creation sub

--- perl/test.pl
+++ perl/test.pl
@@ -9,16 +9,28 @@ use_ok 'GildedRose';
 use_ok 'GildedRose::Item';
 use_ok 'GildedRose::Constants';
 
+# Need to do this to trigger constants import
+use GildedRose::Constants qw(:all);
+
+sub initGR {
+    my($sell_in_days, $quality) = @_;
+    my $items = [
+      GildedRose::Item->new( name => DEX_VEST, sell_in => $sell_in_days, quality => $quality ),
+      GildedRose::Item->new( name => AGED_BRIE, sell_in => $sell_in_days, quality => $quality ),
+      GildedRose::Item->new( name => SULFURAS, sell_in => $sell_in_days, quality => $quality ),
+    ];
+    return GildedRose->new( items => $items );
+}
+
 subtest 'Instantiation Tests' => sub {
     my $sell_in_days = 2;
     my $quality      = 2;
-    my $name         = 'Foo Fighting Sword';
-    my $items = [ GildedRose::Item->new( name => $name, sell_in => $sell_in_days, quality => $quality ) ];
-    my $app = GildedRose->new( items => $items );
-    my $check_item = [ $app->items() ]->[0];
-    is($check_item->name,    $name,         'Item name correct');
-    is($check_item->quality, $quality,      'Item quality correct');
-    is($check_item->sell_in, $sell_in_days, 'Item sell_in correct');
+    my $app          = initGR($sell_in_days, $quality);
+    my $item         = [ $app->items() ]->[0];
+
+    is($item->name,    DEX_VEST,      'Item name correct');
+    is($item->quality, $quality,      'Item quality correct');
+    is($item->sell_in, $sell_in_days, 'Item sell_in correct');
 };
 
 =pod
@@ -27,17 +39,16 @@ subtest 'Instantiation Tests' => sub {
 subtest 'Degradation Tests' => sub {
     my $sell_in_days = 2;
     my $quality      = 2;
-    my $items = [ GildedRose::Item->new( name => 'foo', sell_in => $sell_in_days, quality => $quality ) ];
-    my $app = GildedRose->new( items => $items );
-    my $check_item = [ $app->items() ]->[0];
-    my $name = $check_item->{name};
+    my $app          = initGR($sell_in_days, $quality);
+    my $item         = [ $app->items() ]->[0];
+
     for my $day ( 1..2 ) {
       $app->update_quality();
-      diag("Day: $day");
       my $target_sell_in = $sell_in_days - $day;
       my $target_quality = $quality - $day;
-      is($check_item->sell_in, $target_sell_in, 'sell_in decreased over time correctly' );
-      is($check_item->quality, $target_quality, 'quality decreased over time correctly' );
+
+      is($item->sell_in, $target_sell_in, "sell_in decreased over time correctly on day $day" );
+      is($item->quality, $target_quality, "quality decreased over time correctly on day $day" );
     }
 };
 
@@ -48,20 +59,19 @@ subtest 'Degradation Tests' => sub {
 subtest 'Boundary Tests' => sub {
     my $sell_in_days = 2;
     my $quality      = 1;
-    my $items = [ GildedRose::Item->new( name => 'foo',  sell_in => $sell_in_days, quality => $quality ) ];
+    my $items = [ GildedRose::Item->new( name => DEX_VEST,  sell_in => $sell_in_days, quality => $quality ) ];
     my $app = GildedRose->new( items => $items );
-    my $check_item = [ $app->items() ]->[0];
-    my $name = $check_item->{name};
-    $app->update_quality();
     $app->update_quality();
 
-    cmp_ok($check_item->quality, '>=', 0, "Quality lower bounds: Does not go below 0" );
-    $items = [ GildedRose::Item->new( name => 'foo',  sell_in => $sell_in_days, quality => 60 ) ];
+    my $item = [ $app->items() ]->[0];
+    cmp_ok($item->quality, '>=', 0, "Quality lower bounds: Does not go below 0" );
+
+    $items = [ GildedRose::Item->new( name => DEX_VEST,  sell_in => $sell_in_days, quality => 60 ) ];
     $app = GildedRose->new( items => $items );
     $app->update_quality();
-    my $check_item2 = [ $app->items() ]->[0];
 
-    cmp_ok($check_item2->quality, '<', 50, "Quality upper bounds: Does not go above 50" );
+    my $item2 = [ $app->items() ]->[0];
+    cmp_ok($item2->quality, '<', 50, "Quality upper bounds: Does not go above 50" );
 };
 
 done_testing();

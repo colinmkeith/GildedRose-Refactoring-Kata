c72bd7fe4c74f1b0a0fb1158b10f194615e479e1
Colin Keith <colin.keith@oracle.com>
2022-10-29 11:59:31 +0100

    move testing to new GR::Item

--- perl/test.pl
+++ perl/test.pl
@@ -6,10 +6,10 @@ use warnings;
 use Test::More 0.96;
 
 use_ok 'GildedRose';
-use_ok 'Item';
+use_ok 'GildedRose::Item';
 
 subtest 'foo' => sub {
-    my $items = [ Item->new( name => 'foo', sell_in => 0, quality => 0 ) ];
+    my $items = [ GildedRose::Item->new( name => 'foo', sell_in => 0, quality => 0 ) ];
     my $app = GildedRose->new( items => $items );
     $app->update_quality();
     is( [ $app->items() ]->[0]->{name}, 'foo' );

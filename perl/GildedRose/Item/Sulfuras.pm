bca010e1aa033f40642d4b7a05e1e481473ce5cd
Colin Keith <colin.keith@oracle.com>
2022-10-31 09:59:21 +0000

    expand setter to enforce limits

--- perl/GildedRose/Item/Sulfuras.pm
+++ perl/GildedRose/Item/Sulfuras.pm
@@ -7,12 +7,21 @@ use parent 'GildedRose::Item';
 
 sub quality {
   my($self, $value) = @_;
-  defined($value) && $value =~ m/^-([0-9]+)$/ && return;
+  if( defined($value) ) {
+    # Ignore all reset/incr/decr of values
+    $self->quality && return;
+
+    # Enforce quality limits
+    $value > 50 && ($value = 50);
+    $value <  0 && ($value = 0);
+  }
   return $self->SUPER::quality($value);
 }
 
 sub sell_in {
   my($self, $value) = @_;
+
+  # Is legendary item, so never ages.
   defined($value) && defined($self->sell_in) && return;
   return $self->SUPER::sell_in($value);
 }
